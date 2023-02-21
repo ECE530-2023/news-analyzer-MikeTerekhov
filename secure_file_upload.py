201d64928cd641df1addfe049e08082ea35b4a57
MikeTerekhov <terekhov@bu.edu>
2023-02-20 20:38:34 -0500

    adding more info to function stubs

--- secure_file_upload.py
+++ secure_file_upload.py
@@ -2,7 +2,16 @@ def secure_login():
     return "Login worked or didnt"
 
 def uploader():
+    print("file or image? : ")
     file_name = input("Enter the name of the file you are uploading: ")
 
 def checker(file_name):
-    return "Error related to uploader"
+    if file_name.endswith('.txt'):
+        print('valid')
+    if file_name.endswith('.test'):
+        print('valid')
+    if file_name.lower().endswith(('.png', '.jpg', '.jpeg'))
+        print("valid or not IDK yet")
+    else:
+        return "Error related to uploader"
+
