441739dd728ef2735edcd2a830b5c939ed8ed4d7
MikeTerekhov <terekhov@bu.edu>
2023-03-18 21:24:57 -0400

    data bases construction

--- secure_file_upload.py
+++ secure_file_upload.py
@@ -1,3 +1,9 @@
+import PyPDF2
+import io
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+#implement this using google API
 def secure_login(username, password):
     username_ = "Mike"
     password_ = 123
@@ -7,17 +13,28 @@ def secure_login(username, password):
     return False
     # Errors 001, 002, 006
 
-def uploader(type_in):
-    #type_in = input("file or image? : ")
-    #file_name = input("Enter the name of the file you are uploading: ") 
-    if type_in == "file":
-        return True
-    elif type_in == "image":
-        return True
-    else:
-        return False
-    # Error 008
+def uploader():
+    # prompt user to select a PDF file
+    Tk().withdraw()
+    filename = askopenfilename(filetypes=[("PDF Files", "*.pdf")])
 
+    # read the PDF file
+    with open(filename, "rb") as f:
+        pdf = PyPDF2.PdfReader(f)
+        # iterate over all pages of the PDF file
+        for page_num in range(pdf.numPages):
+            page = pdf.getPage(page_num)
+            # extract text from the page
+            text = page.extractText()
+            # split the text into words
+            words = text.split()
+            # print the words
+            for word in words:
+                print(word)
+    
+
+#currently this function is not needed because uploader() doesnt allow selection
+#of anything that is not PDF format
 def checker(file_name):
     if file_name.endswith('.txt'):
         print('valid')
@@ -30,4 +47,4 @@ def checker(file_name):
         return False
     # Error 001
 
-
+uploader()
